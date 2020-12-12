175e20cc388c2a2c5e6d980d6765202d4c9d1df2
vhermecz <vhermecz@gmail.com>
2020-12-12 11:33:07 +0000

    Updates

---
+++ precache-manifest.6282d3457a30496fb8bfb221ccbc55fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "644dbc831460f21f4931c84116e0bd32",
+    "url": "/aoc-dash-viewer/index.html"
+  },
+  {
+    "revision": "34fe1142acb54b997ea9",
+    "url": "/aoc-dash-viewer/static/css/main.1100813c.chunk.css"
+  },
+  {
+    "revision": "4e8997efb55db6a13b99",
+    "url": "/aoc-dash-viewer/static/js/2.fad91ec0.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/aoc-dash-viewer/static/js/2.fad91ec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34fe1142acb54b997ea9",
+    "url": "/aoc-dash-viewer/static/js/main.05a950b4.chunk.js"
+  },
+  {
+    "revision": "7e9b2ba6a5c469f04354",
+    "url": "/aoc-dash-viewer/static/js/runtime-main.d5bd0b16.js"
+  }
+]);
